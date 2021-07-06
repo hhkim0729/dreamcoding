ac75d17d7f6d82028988d245df3b5cb739074ccf
hhkim0729 <heehkim@student.42seoul.kr>
2021-07-06 15:59:09 +0900

    array-api

--- javascript/array-api.js
+++ javascript/array-api.js
@@ -57,32 +57,99 @@ const students = [
 ];
 
 // Q5. find a student with the score 90
+// 콜백 함수가 첫 번째로 true를 리턴하는 오브젝트를 찾음
 {
+  // let result;
+  // for (student of students) {
+  //   if (student.score === 90) {
+  //     result = student;
+  //   }
+  // }
+  // console.log(result);
+  const result = students.find(student => student.score === 90);
+  console.log(result);
 }
 
 // Q6. make an array of enrolled students
 {
+  // const result = [];
+  // students.forEach(student => {
+  //   if (student.enrolled === true) {
+  //     result.push(student);
+  //   }
+  // });
+  // console.log(result);
+  const result = students.filter(student => student.enrolled === true);
+  console.log(result);
 }
 
 // Q7. make an array containing only the students' scores
 // result should be: [45, 80, 90, 66, 88]
 {
+  // const result = [];
+  // students.forEach(student => result.push(student.score));
+  // console.log(result);
+  const result = students.map(student => student.score);
+  console.log(result);
 }
 
+console.clear();
 // Q8. check if there is a student with the score lower than 50
 {
+  // let result = false;
+  // students.forEach(student => {
+  //   if (student.score < 50) {
+  //     result = true;
+  //   }
+  // });
+  // console.log(result);
+
+  // 콜백 함수의 리턴값이 true인 값이 있는지 없는지 확인
+  const result = students.some(student => student.score < 50);
+  console.log(result);
+
+  // 콜백 함수의 모든 리턴값이 true인지 확인
+  const result2 = !students.every(student => student.score < 50);
+  console.log(result2);
 }
 
+console.clear();
 // Q9. compute students' average score
+// 리턴하는 값을 prev로 전달 (값을 누적할 때 사용)
+// reduceRight은 배열의 끝부터
 {
+  // let sum = 0;
+  // students.forEach(student => (sum += student.score));
+  // const average = sum / students.length;
+  // console.log(average);
+  const result = students.reduce((prev, curr) => prev + curr.score, 0);
+  console.log(result / students.length);
 }
 
 // Q10. make a string containing all the scores
 // result should be: '45, 80, 90, 66, 88'
 {
+  // let result = '';
+  // students.forEach((student, index) => {
+  //   result += student.score;
+  //   if (index !== students.length - 1) {
+  //     result += ', ';
+  //   }
+  // });
+  // console.log(result);
+  const result = students
+    .map(student => student.score)
+    .filter(score => score >= 50)
+    .join(', ');
+  console.log(result);
 }
 
 // Bonus! do Q10 sorted in ascending order
 // result should be: '45, 66, 80, 88, 90'
 {
+  const result = students
+    .map(student => student.score)
+    .sort((a, b) => b - a)
+    .join(', ');
+  console.log(result);
 }
